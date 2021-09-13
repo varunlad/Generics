80bd66acc46289bef13258fa527c8a4e5f91ce6e
varun <varunlad5@gmail.com>
2021-09-13 13:51:17 +0530

    [varun] user case one find maximum integer using compare to method

---
+++ Generic058/CompareMethod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic058
+{
+    class CompareMethod
+    {
+        public static int MaxNumber(int first,int second,int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("Tow or more numbers are Same");
+                return 0;            }
+
+        }
+    }
+}
---
+++ Generic058/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic058
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" The maximum  number is :{0} " ,CompareMethod.MaxNumber(13, 34, 51));
+            Console.ReadLine();        
+        }
+    }
+}
